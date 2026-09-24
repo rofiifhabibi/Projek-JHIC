4e25fbd69c60f0e16a5b9a97c2bf7bae202a63f5
Muhammad Rofiif Arifian <muhammadrofiif38@gmail.com>
2026-09-24 20:25:19 +0700

    mode sedikit pecut karena malas hehe

---
+++ Backend/app/Models/Subject.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $table = 'subjects';
+    protected $primaryKey = 'subject_id';
+
+    protected $fillable = [
+        'code',
+        'name',
+    ];
+
+    public function schedules()
+    {
+        return $this->hasMany(Schedule::class, 'subject_id', 'subject_id');
+    }
+}
